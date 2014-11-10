6dbaee8ba3db9394ea7dba7f875e30e623c2f7d7
Stijn Deschuymer <stijndeschuymer@gmail.com>
2014-11-10 20:34:30 +0100

    local example finished

---
+++ server/boot/create-members.js
@@ -0,0 +1,25 @@
+var members = [
+  {userid:'E863EA61-C07D-47EE-95E6-00009BD074ED', name: 'Customer A', age: 21},
+  {userid:'A25B7B45-5F44-4114-AEA9-0001E7C0F132', name: 'Customer B', age: 22},
+  {userid:'A5026114-F799-4AAD-8CA6-0003BE9A41E4', name: 'Customer C', age: 23}
+];
+
+module.exports = function(server) {
+  var dataSource = server.dataSources.db;
+  var Model = server.models.Member;
+  dataSource.automigrate('Member', function(er) {
+    if (er) throw er;
+    //create sample data
+    var count = members.length;
+    members.forEach(function(member) {
+      Model.create(member, function(er, result) {
+        if (er) return;
+        console.log('Record created:', result);
+        count--;
+        if (count === 0) {
+          console.log('done');
+        }
+      });
+    });
+  });
+};
---
+++ server/boot/create-memberships.js
@@ -0,0 +1,25 @@
+var memberships = [
+  {userid:'E863EA61-C07D-47EE-95E6-00009BD074ED', ismember: true, type: 'renter'},
+  {userid:'A25B7B45-5F44-4114-AEA9-0001E7C0F132', ismember: true, type: 'owner'},
+  {userid:'A5026114-F799-4AAD-8CA6-0003BE9A41E4', ismember: false, type: 'renter'}
+];
+
+module.exports = function(server) {
+  var dataSource = server.dataSources.db;
+  dataSource.automigrate('Membership', function(er) {
+    if (er) throw er;
+    var Model = server.models.Membership;
+    //create sample data
+    var count = memberships.length;
+    memberships.forEach(function(membership) {
+      Model.create(membership, function(er, result) {
+        if (er) return;
+        console.log('Record created:', result);
+        count--;
+        if (count === 0) {
+          console.log('done');
+        }
+      });
+    });
+  });
+};
